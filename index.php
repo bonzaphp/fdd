3bf7fbfd64689abb8861b66035270297260e46a5
yang <y.shi.guo@gmail.com>
2019-07-03 14:35:59 +0800

    统一调用入口，通过魔术方法，简化调用代码

--- index.php
+++ index.php
@@ -19,12 +19,18 @@ $options =[
 ];
 $fdd = new Fdd(new FddApi3($options));
 
-$res = $fdd->accountRegister('456');
+//$res = $fdd->accountRegister('456');
 
+
+//$customer_id = $res['data'];
+$customer_id = '4233C1588C0EA5E12109E7D90A5DC760';
+$result = $fdd->getPersonVerifyUrl($customer_id,'http://fdd.ngrok.bonza.cn/notify.php');
+$result['data']['url'] = base64_decode($result['data']['url']);
 
 //$obj =new  \ReflectionClass('bonza\fdd\FddApi2');
 
 //var_dump($obj);die;
 //$res  = $obj->getMethods();
 
-echo (json_encode($res));
+echo (json_encode($result));
+//echo (json_encode($res));
