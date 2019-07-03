4291fa17c64ba3ab4e828add5815052d0386414e
yang <y.shi.guo@gmail.com>
2019-07-03 17:33:54 +0800

    完善说明文档 调用逻辑

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 
-### 法大大接口2.0
+## 法大大接口3.0
 
-1. 使用示例在index.php
+### 1. 使用示例在
 
-# fdd
-法大大接口
+```php
+
+$options =[
+    'appId' => 'xxxxxx',
+    'appSecret'=>'xxxxxxxxxxxxx',
+    'baseUrl' => 'https://testapi.fadada.com:8443/api/',
+    'version'=>'3.0'
+];
+$fdd = new Fdd(new FddApi3($options));
+
+/** @return array  */
+
+//$res = $fdd->accountRegister('456');
+
+
+```
+
--- src/Fdd.php
+++ src/Fdd.php
@@ -8,18 +8,25 @@
 
 namespace bonza\fdd;
 
-use bonza\fdd\interfaces\FddInterface;
+use bonza\fdd\api\FddApi3;
 
 class Fdd
 {
 
     private $fdd = null;
 
-    public function __construct(FddInterface $fdd)
+    public function __construct($options)
     {
-        $this->fdd = $fdd;
+        $this->fdd = new FddApi3($options);
     }
 
+    /**
+     * 代理接口请求到具体的实例
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @author bonzaphp@gmail.com
+     */
     public function __call($name, $arguments)
     {
         return $this->fdd->$name(...$arguments);
